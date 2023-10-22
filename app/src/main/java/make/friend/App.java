9230cca86d106baefa9199bae731226e88deb28e
Donghak Kim <6283hz@Donghaks-MacBook-Pro.local>
2023-10-22 18:11:03 +0900

    JDBC MySQL 의존성 추가와 테스트 코드

--- app/src/main/java/make/friend/App.java
+++ app/src/main/java/make/friend/App.java
@@ -3,12 +3,46 @@
  */
 package make.friend;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class App extends JFrame{
+    final int WIDTH = 300, HEIGHT = 300;
+
+    App(){
+        JButton hello = new JButton("넌 뭐야");
+        add(hello);
+
+        setSize(WIDTH, HEIGHT);
+        setVisible(true);
+        setTitle("테스트");
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        try {
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306",
+                    "root",
+                    "1234"
+            );
+
+            java.sql.Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(
+                    "show databases"
+            );
+
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        new App();
     }
 }
