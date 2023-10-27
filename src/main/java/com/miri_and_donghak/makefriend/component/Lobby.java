6d94cfa2557c87fabeb5c2a4e3c7c71295f70347
Donghak Kim <6283hz@Donghaks-MacBook-Pro.local>
2023-10-28 00:15:40 +0900

    생성자 최적화

--- src/main/java/com/miri_and_donghak/makefriend/component/Lobby.java
+++ src/main/java/com/miri_and_donghak/makefriend/component/Lobby.java
@@ -14,15 +14,11 @@ public class Lobby extends JFrame{
     private static Dimension fullsize = Toolkit.getDefaultToolkit().getScreenSize();
 
     public Lobby(){
-        BufferedImage cursorImg = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
-        Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(cursorImg, new Point(0, 0), "blank cursor");
-        getContentPane().setCursor(blankCursor);
-
-
-        JPanel lobby = new JPanel(new BorderLayout());
-
+        setBlankCursor();
         JLabel point = new JLabel("ㅎㅇ");
         point.setFont(FontUtil.getFonts());
+
+        JPanel lobby = new JPanel(new BorderLayout());
 
         lobby.addMouseMotionListener(new MouseAdapter() {
             int count = 0;
@@ -32,32 +28,55 @@ public class Lobby extends JFrame{
             }
         });
 
-        JPanel buttons = new JPanel();
+        lobby.add(point);
+        lobby.add(createButtons(), BorderLayout.SOUTH);
 
+        add(lobby);
+
+        defaultSetting();
+    }
+
+    private void setBlankCursor(){
+        BufferedImage cursorImg = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+        Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(cursorImg, new Point(0, 0), "blank cursor");
+        getContentPane().setCursor(blankCursor);
+    }
+
+    private void defaultSetting(){
+        setTitle("동학이 친구 만들기");
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(fullsize);
+    }
+
+    private JButton createMakeFriendButton(){
         JButton makeFriendButton = ButtonUtil.createButton("친구 만들기");
-        JButton showMyFriend = ButtonUtil.createButton("친구한테 가기");
 
         makeFriendButton.addActionListener(l -> {
             new MakeFriend();
             setVisible(false);
         });
 
-        showMyFriend.addActionListener(l ->{
+        return makeFriendButton;
+    }
+    private JPanel createButtons(){
+        JButton makeFriendButton = ButtonUtil.createButton("친구 만들기");
+        JButton showMyFriendButton = ButtonUtil.createButton("친구한테 가기");
+
+        makeFriendButton.addActionListener(l -> {
+            new MakeFriend();
+            setVisible(false);
+        });
+        showMyFriendButton.addActionListener(l ->{
             new ShowMyFriends();
             setVisible(false);
         });
 
+        JPanel buttons = new JPanel();
+
         buttons.add(makeFriendButton);
-        buttons.add(showMyFriend);
+        buttons.add(showMyFriendButton);
 
-        lobby.add(point);
-        lobby.add(buttons, BorderLayout.SOUTH);
-
-        setTitle("동학이 친구 만들기");
-        setVisible(true);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(fullsize);
-
-        add(lobby);
+        return buttons;
     }
 }
