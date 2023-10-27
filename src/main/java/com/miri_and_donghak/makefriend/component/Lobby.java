9e76df866e08ec412dd2a212731ece7a5c1fc708
Donghak Kim <6283hz@Donghaks-MacBook-Pro.local>
2023-10-27 23:38:18 +0900

    폰트 사이즈 변경 및 마우스 커서 대신 간단한 텍스트가 뜸

--- src/main/java/com/miri_and_donghak/makefriend/component/Lobby.java
+++ src/main/java/com/miri_and_donghak/makefriend/component/Lobby.java
@@ -2,15 +2,37 @@ package com.miri_and_donghak.makefriend.component;
 
 import com.miri_and_donghak.makefriend.util.ButtonUtil;
 import com.miri_and_donghak.makefriend.util.FontUtil;
+import jakarta.persistence.Lob;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 
 public class Lobby extends JFrame{
     private static Dimension fullsize = Toolkit.getDefaultToolkit().getScreenSize();
 
     public Lobby(){
-        JPanel lobby = new JPanel(new GridBagLayout());
+        BufferedImage cursorImg = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+        Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(cursorImg, new Point(0, 0), "blank cursor");
+        getContentPane().setCursor(blankCursor);
+
+
+        JPanel lobby = new JPanel(new BorderLayout());
+
+        JLabel point = new JLabel("ㅎㅇ");
+        point.setFont(FontUtil.getFonts());
+
+        lobby.addMouseMotionListener(new MouseAdapter() {
+            int count = 0;
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                point.setLocation(e.getX(), e.getY() - 380);
+            }
+        });
+
+        JPanel buttons = new JPanel();
 
         JButton makeFriendButton = ButtonUtil.createButton("친구 만들기");
         JButton showMyFriend = ButtonUtil.createButton("친구한테 가기");
@@ -25,8 +47,11 @@ public class Lobby extends JFrame{
             setVisible(false);
         });
 
-        lobby.add(makeFriendButton);
-        lobby.add(showMyFriend);
+        buttons.add(makeFriendButton);
+        buttons.add(showMyFriend);
+
+        lobby.add(point);
+        lobby.add(buttons, BorderLayout.SOUTH);
 
         setTitle("동학이 친구 만들기");
         setVisible(true);
