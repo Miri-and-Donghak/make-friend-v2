3eaceea69c6e2aaa1fd5b1f40e3f55b5d82d3a32
parkmiri <parkmiri0722@gmail.com>
2023-11-09 15:01:17 +0900

    update Background Image

--- src/main/java/com/miri_and_donghak/makefriend/component/Lobby.java
+++ src/main/java/com/miri_and_donghak/makefriend/component/Lobby.java
@@ -1,6 +1,7 @@
 package com.miri_and_donghak.makefriend.component;
 
 import com.miri_and_donghak.makefriend.util.ButtonUtil;
+import com.miri_and_donghak.makefriend.util.CursorUtil;
 import com.miri_and_donghak.makefriend.util.FontUtil;
 import jakarta.persistence.Lob;
 
@@ -11,39 +12,48 @@ import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.net.URL;
 
-public class Lobby extends JFrame{
+public class Lobby extends JFrame {
     private static Dimension fullsize = Toolkit.getDefaultToolkit().getScreenSize();
+    private Image backgroundImage;
 
-    public Lobby(){
+    public Lobby() {
+        setTitle("동학이 친구 만들기");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setUndecorated(true);
 
-        JPanel lobby = new JPanel(new BorderLayout());
+        loadImage();
 
-        lobby.setCursor(customcursor());
+        JPanel lobby = new JPanel(new BorderLayout()) {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                if (backgroundImage != null) {
+                    g.drawImage(backgroundImage, -300, 0, this);
+                }
+            }
+        };
+
+        lobby.setCursor(CursorUtil.customCursor());
         lobby.add(createButtons(), BorderLayout.SOUTH);
 
         add(lobby);
-        defaultSetting();
-    }
-
-
-    private void defaultSetting(){
-        setTitle("동학이 친구 만들기");
         setVisible(true);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(fullsize);
     }
 
-    public Cursor customcursor() {
-        Toolkit tk = Toolkit.getDefaultToolkit();
-        Image cursorimage=tk.getImage("src/main/resources/images/cursor.png");
-
-        Point point = new Point(0, 0);
-        Cursor cursor = tk.createCustomCursor(cursorimage, point, "haha");
-
-        return cursor;
+    private void loadImage() {
+        try {
+            URL image = getClass().getResource("/images/background.png");
+            if (image != null) {
+                backgroundImage = new ImageIcon(image).getImage();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    private JPanel createButtons(){
+
+    private JPanel createButtons() {
         JButton makeFriendButton = ButtonUtil.createButton("친구 만들기");
         JButton showMyFriendButton = ButtonUtil.createButton("친구한테 가기");
 
@@ -51,7 +61,7 @@ public class Lobby extends JFrame{
             new MakeFriend();
             setVisible(false);
         });
-        showMyFriendButton.addActionListener(l ->{
+        showMyFriendButton.addActionListener(l -> {
             new ShowMyFriends();
             setVisible(false);
         });
